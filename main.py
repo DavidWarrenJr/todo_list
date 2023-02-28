491822aa301b5923109da99491e7a04138f74263
David <davidwarrenjr90@gmail.com>
2023-02-27 18:47:04 -0600

    Create column and cards

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+all_task = []
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_task=all_task)
 
 
 if __name__ == "__main__":
