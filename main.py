814571de53111427104b7d33ade1b4f5246d3402
David <davidwarrenjr90@gmail.com>
2023-03-04 11:14:41 -0600

    Add database

--- main.py
+++ main.py
@@ -1,15 +1,99 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
 
-all_task = []
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String)
+    title = db.Column(db.String(250))
+    description = db.Column(db.String)
+    due_year = db.Column(db.Integer)
+    due_month = db.Column(db.String)
+    due_day = db.Column(db.Integer)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
+    all_task = db.session.query(Todo).all()
     return render_template("index.html", all_task=all_task)
 
 
+def get_due_month_string(month: str) -> str:
+    if month == "01":
+        return "Jan"
+    if month == "02":
+        return "Feb"
+    if month == "03":
+        return "Mar"
+    if month == "04":
+        return "Apr"
+    if month == "05":
+        return "May"
+    if month == "06":
+        return "June"
+    if month == "07":
+        return "July"
+    if month == "08:":
+        return "Aug"
+    if month == "09":
+        return "Sept"
+    if month == "10":
+        return "Oct"
+    if month == "11":
+        return "Nov"
+    if month == "12":
+        return "Dec"
+
+
+def get_due_day(day: str) -> str:
+    if day[0] == "0":
+        return day[1]
+    else:
+        return day
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        print(request.form.get("cancel-btn"))
+        if request.form.get("cancel-btn") == "True":
+            return redirect(url_for("home"))
+
+        image = request.form.get("image-input")
+        title = request.form.get("title-input")
+        description = request.form.get("description-input")
+        due_date = request.form.get("date-input")
+
+        if due_date:
+            year = due_date.split('-')[0]
+            month = due_date.split('-')[1]
+            day = due_date.split('-')[2]
+
+            task = Todo(image=image,
+                        title=title,
+                        description=description,
+                        due_year=year,
+                        due_month=get_due_month_string(month),
+                        due_day=get_due_day(day))
+        else:
+            task = Todo(image=image,
+                        title=title,
+                        description=description)
+
+        db.session.add(task)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
---
+++ static/js/add_script.js
@@ -0,0 +1,35 @@
+const image_label = document.getElementById("image-label");
+const title_label = document.getElementById("title-label");
+const description_label = document.getElementById("description-label");
+const due_date_label = document.getElementById("due_date_label");
+const cancel_btn = document.getElementById("cancel-btn");
+
+const show_image_input = () => {
+    const image_input = document.getElementById("image-input");
+    image_input.classList.toggle('show');
+}
+
+const show_title_input = () => {
+    const title_input = document.getElementById("title-input");
+    title_input.classList.toggle("show");
+}
+
+const show_description_input = () => {
+    const description_input = document.getElementById("description-input");
+    description_input.classList.toggle("show");
+}
+
+const show_due_date_input = () => {
+    const due_date_input = document.getElementById("due_date_input");
+    due_date_input.classList.toggle("show");
+}
+
+const cancel = (event) => {
+    event.target.setAttribute("value", "True");
+}
+
+image_label.addEventListener('click', show_image_input);
+title_label.addEventListener('click', show_title_input);
+description_label.addEventListener('click', show_description_input);
+due_date_label.addEventListener('click', show_due_date_input);
+cancel_btn.addEventListener('click', cancel);
